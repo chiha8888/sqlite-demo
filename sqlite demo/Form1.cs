b086223e5dc3322610275e4882c061050c23606e
dora <chiha8888@gmail.com>
2018-06-10 22:47:27 +0800

    first commit

---
+++ sqlite demo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sqlite_demo
+{
+    public partial class Form1 : Form
+    {
+        SqlAccess sqlAccess=new SqlAccess();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(sqlAccess.insert(textBox1.Text)==false)
+                MessageBox.Show("insert failure.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int c=sqlAccess.count(textBox1.Text);
+            MessageBox.Show(c.ToString()+"個");
+        }
+    }
+}
---
+++ sqlite demo/SqlAccess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SQLite;
+
+namespace sqlite_demo
+{
+    class SqlAccess
+    {
+        public bool insert(string name)
+        {
+            
+            if (!File.Exists(Application.StartupPath+@"\food.db"))
+            {
+                return false;
+            }
+            using (SQLiteConnection sqlite_connect=new SQLiteConnection("Data source=food.db"))
+            {
+                sqlite_connect.Open();
+                string cmdString = "insert into food(name) values ('" + name + "')";
+                SQLiteCommand cmd = new SQLiteCommand(cmdString, sqlite_connect);
+                Convert.ToInt32(cmd.ExecuteNonQuery());
+                sqlite_connect.Close();
+            }
+            return true;
+        }
+        public int count(string name)
+        {
+            int re;
+            if (!File.Exists(Application.StartupPath + @"\food.db"))
+            {
+                return -1;
+            }
+            using (SQLiteConnection sqlite_connect = new SQLiteConnection("Data source=food.db"))
+            {
+                sqlite_connect.Open();
+                string cmdString = "select count(*) from food where name='" + name + "'";
+                SQLiteCommand cmd = new SQLiteCommand(cmdString, sqlite_connect);
+                re = Convert.ToInt32(cmd.ExecuteScalar());
+                sqlite_connect.Close();
+            }
+            return re;
+        }
+
+    }
+}
